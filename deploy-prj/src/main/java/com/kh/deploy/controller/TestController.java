ef2e388101911398521cac6ca3cc622cda29794a
ansgmlrjs <moonheegun1208@gmail.com>
2026-06-26 20:40:15 +0900

    11

--- deploy-prj/src/main/java/com/kh/deploy/controller/TestController.java
+++ deploy-prj/src/main/java/com/kh/deploy/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@RequestMapping("/")
 	public String home() {
-		return "welcome!!!!!";
+		return "welcome please!!!!!";
 	}
 }
